d24bace56f74ef3fa1ae8871f159e8d72d5dd2e0
craj68168 <craj68168@gmail.com>
2026-09-06 15:51:20 +0545

    feat:code refactor

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
